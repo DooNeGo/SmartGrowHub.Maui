e6f0dd5e626eaf5f962b433c9465dfa89725acd5
Матвей Кострома <matveyprostomac@gmail.com>
2024-12-18 01:18:00 +0300

    Improve creation of eff without Option

--- SmartGrowHub.Maui/SmartGrowHub.Maui/Services/Extensions/SecureStorageExtensions.cs
+++ SmartGrowHub.Maui/SmartGrowHub.Maui/Services/Extensions/SecureStorageExtensions.cs
@@ -5,8 +5,9 @@ public static class SecureStorageExtensions
     public static Eff<string> GetEff(this ISecureStorage secureStorage, string key,
         CancellationToken cancellationToken) =>
         liftEff(() => secureStorage.GetAsync(key).WaitAsync(cancellationToken))
-            .Bind(value => Optional(value)
-                .ToEff(Error.New($"No value found in Secure storage for the key: {key}")));
+            .Bind(value => value is null
+                ? FailEff<string>(Error.New($"No value found in Secure storage for the key: {key}"))
+                : SuccessEff(value));
 
     public static Eff<bool> RemoveEff(this ISecureStorage secureStorage, string key) =>
         liftEff(() => secureStorage.Remove(key));
